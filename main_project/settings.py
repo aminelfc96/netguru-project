f83663f2a8bcd23a71f10e6ce89f63afa28408af
Amine <64668497+aminelfc96@users.noreply.github.com>
2022-01-27 18:55:54 +0100

    netguru project

--- main_project/settings.py
+++ main_project/settings.py
@@ -95,7 +95,7 @@ WSGI_APPLICATION = 'main_project.wsgi.application'
 
 DATABASES = {}
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+DATABASES['default'] = dj_database_url.config()
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
